9b07f5c7cd72c7259908afb09c1a814cbc4b5f11
clarkaryzack <clarkaryzack@gmail.com>
2017-07-25 15:51:42 -0400

    Broken branch.

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 
-import ShopHeader from './ShopHeader'
-import LikeBanner from './LikeBanner'
-import ProductImage from './ProductImage'
-import PurchaseInfo from './PurchaseInfo'
-import ProdDesc from './ProdDesc'
-import ShopSidebar from './ShopSidebar'
+import ShopHeader from "./ShopHeader";
+import LikeBanner from "./LikeBanner";
+import ProductImage from "./ProductImage";
+import PurchaseInfo from "./PurchaseInfo";
+import ProdDesc from "./ProdDesc";
+import ShopSidebar from "./ShopSidebar";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       results: {}
-    }
+    };
   }
   componentDidMount() {
-    fetch('https://openapi.etsy.com/v2/listings/175112598?includes=Shop/Listings/MainImage&api_key=nrfza0h31bu4g5biq6bq6g4c').then(response => response.json()).then(response => {
-      this.state = {
-        results: response.results
-      }
-      console.log(this.state);
-    })
+    fetch(
+      "https://openapi.etsy.com/v2/listings/175112598?includes=Shop/Listings/MainImage&api_key=nrfza0h31bu4g5biq6bq6g4c"
+    )
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ results: response.results });
+        console.log(this.state.results);
+      });
   }
   render() {
     return (
       <div className="App">
         <div className="row justify-content-center">
           <div className="row col-10 justify-content-center">
-            <ShopHeader />
+            <ShopHeader results={this.state.results} />
           </div>
         </div>
         <div className="row justify-content-center">
--- src/components/ShopHeader.js
+++ src/components/ShopHeader.js
@@ -1,54 +1,65 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../styles/App.css";
 
 class ShopHeader extends Component {
-  constructor() {
-    super();
-    this.state = {
-      shop: {}
-    };
-  }
-  componentDidMount() {
-    fetch(
-      "https://openapi.etsy.com/v2/shops/listing/175112598?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
-    )
-      .then(response => response.json())
-      .then(response => {
-        console.log(response);
-				let shopCards = response.results[0].Listings.map(shopCard => {
-					return(
-						<div>
+	// constructor(props) {
+	// 	super(props);
+	// }
+	// componentDidMount() {
+	//   fetch(
+	//     "https://openapi.etsy.com/v2/shops/listing/175112598?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
+	//   )
+	//     .then(response => response.json())
+	//     .then(response => {
+	//       console.log(response);
+	// 			let shopCards = response.results[0].Listings.map(shopCard => {
+	// 				return(
+	// 					<div>
+	//
+	// 					</div>
+	// 				)
+	// 			})
+	//       this.setState({
+	//         shop: {
+	//           shop_id: response.results[0].shop_id,
+	//           shop_name: response.results[0].shop_name,
+	//           shop_url: response.results[0].url
+	//         }
+	//       });
+	// fetch(
+	//   "https://openapi.etsy.com/v2/shops/"+this.state.shop.shop_id+"?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
+	// )
+	//   .then(response => response.json())
+	//   .then(
+	// 		console.log(response)
+	// 	);
+	// })
+	//     .catch(error => {
+	//       console.error(error);
+	//     });
+	// }
 
-						</div>
-					)
-				})
-        this.setState({
-          shop: {
-            shop_id: response.results[0].shop_id,
-            shop_name: response.results[0].shop_name,
-            shop_url: response.results[0].url
-          }
-        });
-        // fetch(
-        //   "https://openapi.etsy.com/v2/shops/"+this.state.shop.shop_id+"?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
-        // )
-        //   .then(response => response.json())
-        //   .then(
-				// 		console.log(response)
-				// 	);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className="ShopHeader col-12 bg-faded">
-        <p>ShopHeader code needed.</p>
-      </div>
-    );
-  }
+	render() {
+		console.log(this.state);
+		// console.log(this.state.results);
+		// console.log(this.state.results[0].Shop);
+		// console.log(this.props.results.results[0]);
+		// console.log(this.props.results.results[0].Shop)
+		let results = this.props.results;
+		// let shoptitle = this.props.results[0].Shop.shop_name;
+		// console.log(results)
+		// console.log(shoptitle)
+		// let renderresults = results.map((result) => {
+		//
+		// })
+		return (
+			<div className="ShopHeader col-12 bg-faded">
+				<p>
+					ShopHeader code needed.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default ShopHeader;
